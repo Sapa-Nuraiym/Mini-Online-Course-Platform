c08790455c2c444e3e955632eca2f36c3efedb9e
Sapa-Nuraiym <sapanuraiym@gmail.com>
2026-05-28 09:58:53 +0500

    Day 2: Flask setup and project structure

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'dev-secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///courses.db'
+    db.init_app(app)
+    return app
